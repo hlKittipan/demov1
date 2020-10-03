e5be9818b7696875ce42ce9cdca200aa94c60a95
Mr.Kittipan Sukhantharot <hlKittipan@users.noreply.github.com>
2020-10-04 00:01:34 +0700

    init

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <h2 class="logo">
+      Website
+    </h2>
+    <input id="chk" type="checkbox">
+    <label for="chk" class="show-menu-btn">
+      <span><font-awesome-icon :icon="['fas', 'ellipsis-h']" /></span>
+    </label>
+    <ul class="menu">
+      <a href="#">Home</a>
+      <a href="#">Register</a>
+      <a href="#">Login</a>
+      <a href="#">Promotion</a>
+      <a href="#">Contact us</a>
+      <label for="chk" class="hide-menu-btn">
+        <font-awesome-icon :icon="['fas', 'times']" />
+      </label>
+    </ul>
+  </div>
+</template>
